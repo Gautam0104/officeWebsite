c97923131b459ccc81505004e3becea066c8d877
Gautam shukla <gautamshukla263@gmail.com>
2023-06-05 17:52:50 +0530

    account page completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import './style.css';
+
+
+
+
+// js for toggle form
+
+let LogForm = document.getElementById("LogForm");
+let RegForm = document.getElementById("RegForm");
+let Indicator = document.getElementById("Indicator");
+
+function register(){
+    RegForm.style.transform = "translateX(0px)";
+    LogForm.style.transform = "translateX(0px)";
+}
+
+function login(){
+    RegForm.style.transform = "translateX(300px)";
+    LogForm.style.transform = "translateX(300px)";
+}
